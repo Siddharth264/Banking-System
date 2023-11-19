efb9305b2c5fe9b7b867cc0565582cab426e391b
Siddharth Singh <sidsingh264@gmail.com>
2023-11-19 11:22:20 +0530

    Production server added

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://banking-system-psi.vercel.app",
         secure: false,
       },
     },
