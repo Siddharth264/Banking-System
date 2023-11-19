1afa74e29e68b89cb5f12b1cebead375474d54cd
Siddharth Singh <sidsingh264@gmail.com>
2023-11-19 15:26:20 +0530

    aaaaa

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://banking-system-two.vercel.app",
-        secure: false,
+        secure: true,
       },
     },
   },
